b69c5c66363af53ea21133d74e819f782845c867
Pasqualino Titto Assini <tittoassini@gmail.com>
2019-06-03 10:23:03 +0200

    added tests

--- src/Data/List/Extra.hs
+++ src/Data/List/Extra.hs
@@ -1,10 +1,13 @@
 {- Additional List functions -}
 module Data.List.Extra (list) where
 
--- from the 'extra' package    
--- > list 1 (\v _ -> v - 2) [5,6,7] == 3
--- > list 1 (\v _ -> v - 2) []      == 1
--- > \nil cons xs -> maybe nil (uncurry cons) (uncons xs) == list nil cons xs    
+{-| from the 'extra' package    
+>>> list 1 (\v _ -> v - 2) []
+1
+
+>>> list 1 (\v _ -> v - 2) [5,6,7]
+3
+-}
 list :: b -> (a -> [a] -> b) -> [a] -> b
 list nil _    [] = nil
 list _   cons (x:xs) = cons x xs
