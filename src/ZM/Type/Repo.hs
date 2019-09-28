8935607cd79e03e48e078dd77767eb35fdbf654c
Pasqualino Titto Assini <tittoassini@gmail.com>
2019-09-28 10:26:07 +0200

    broken down repo protocol in individual functions

--- src/ZM/Type/Repo.hs
+++ src/ZM/Type/Repo.hs
@@ -1,21 +1,44 @@
-{-# LANGUAGE DeriveAnyClass    #-}
-{-# LANGUAGE DeriveGeneric     #-}
-module ZM.Type.Repo (RepoProtocol(..)) where
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric  #-}
 
-import Prelude hiding (String)
+module ZM.Type.Repo
+    --RepoProtocol(..)
+ where
+
+import           Data.Flat
 import           Data.Model
-import Data.Flat
-import ZM.Type.String
-import ZM
+import           Prelude    hiding (String)
 
-{-|
-A (simplistic) protocol to permanently store and retrieve ADT definitions.
--}
-data RepoProtocol = Record AbsADT                      -- ^Permanently record an absolute type
-                  | Solve AbsRef                       -- ^Retrieve the absolute type
-                  | Solved AbsRef AbsADT               -- ^Return the absolute type identified by an absolute reference
-                  | AskDataTypes                       -- ^Request the list of all known data types
-                  | KnownDataTypes [(AbsRef, AbsADT)]  -- ^Return the list of all known data types
-                  | AskDataTypesRefs                       -- ^Request the list of all known data types references (absolute references and names)
-                  | KnownDataTypesRefs [(AbsRef, String)]  -- ^Return the list of all known data types
-  deriving (Eq, Ord, Show, Generic, Flat, Model)
+-- import           ZM
+-- import           ZM.Type.String
+-- {-|
+-- A (simplistic) protocol to permanently store and retrieve ADT definitions.
+-- -}
+-- data RepoProtocol = Record AbsADT                      -- ^Permanently record an absolute type
+--                   | Solve AbsRef                       -- ^Retrieve the absolute type
+--                   | Solved AbsRef AbsADT               -- ^Return the absolute type identified by an absolute reference
+--                   | AskDataTypes                       -- ^Request the list of all known data types
+--                   | KnownDataTypes [(AbsRef, AbsADT)]  -- ^Return the list of all known data types
+--                   | AskDataTypesRefs                       -- ^Request the list of all known data types references (absolute references and names)
+--                   | KnownDataTypesRefs [(AbsRef, String)]  -- ^Return the list of all known data types
+--   deriving (Eq, Ord, Show, Generic, Flat, Model)
+-- Ask for all known values of type a
+data AllKnown a =
+  AllKnown
+  deriving (Eq, Ord, Show, Generic, Flat)
+
+instance Model a => Model (AllKnown a)
+
+-- Record a value of type a
+data Record a =
+  Record a
+  deriving (Eq, Ord, Show, Generic, Flat)
+
+instance (Model a) => Model (Record a)
+
+-- Solve a reference of type to the corresponding value of type to if it exists.
+data Solve ref to =
+  Solve ref
+  deriving (Eq, Ord, Show, Generic, Flat)
+
+instance (Model a, Model b) => Model (Solve a b)
