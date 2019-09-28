96846f9552c4b1af46344af86f2db714ae359148
Pasqualino Titto Assini <tittoassini@gmail.com>
2019-09-28 10:29:22 +0200

    formatting

--- src/ZM/Dynamic.hs
+++ src/ZM/Dynamic.hs
@@ -2,16 +2,16 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
 -- |Dynamical decoding of serialised typed values
-module ZM.Dynamic(
-  decodeAbsTypeModel
-  ,typeDecoder
-  ,typeDecoderMap
-  ,MapTypeDecoder
+module ZM.Dynamic
+  ( decodeAbsTypeModel
+  , typeDecoder
+  , typeDecoderMap
+  , MapTypeDecoder
   ) where
 
-import qualified Data.ByteString      as B
+import qualified Data.ByteString as B
 import           Data.Flat
-import qualified Data.Map             as M
+import qualified Data.Map        as M
 import           Data.Model
 import           ZM.Transform
 import           ZM.Types
@@ -20,8 +20,9 @@ import           ZM.Types
 -- >>> import ZM.Abs
 -- >>> import ZM.Pretty
 -- >>> import ZM.Pretty.Value
-
+-- >>> import Data.Word
 {-| Decode a Flat encoded value with a known type model to the corresponding Value.
+
 
 >>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Bool)) (flat True)
 Right (Value {valType = TypeCon (AbsRef (SHAKE128_48 48 111 25 129 180 28)), valName = "True", valBits = [True], valFields = []})
@@ -30,6 +31,34 @@ Same but prettier:
 
 >>> prettyShow <$> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Bool)) (flat True)
 Right "True"
+
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy (Maybe Bool))) (flat $ Just True)
+Right (Value {valType = TypeApp (TypeCon (AbsRef (SHAKE128_48 218 104 54 119 143 212))) (TypeCon (AbsRef (SHAKE128_48 48 111 25 129 180 28))), valName = "Just", valBits = [True], valFields = [Value {valType = TypeCon (AbsRef (SHAKE128_48 48 111 25 129 180 28)), valName = "True", valBits = [True], valFields = []}]})
+
+>>> prettyShow <$> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Word8)) (flat (11 :: Word8))
+Right "11"
+
+If we use the wrong type we get an error:
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Word8)) (flat 1.1)
+Left (TooMuchSpace ...
+
+or not, if the binary sequence happens to have the same length of a value in the wrong type:
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Word8)) (flat (11::Int))
+Right (Value {valType = TypeCon (AbsRef (SHAKE128_48 177 244 106 73 200 248)), valName = "V22", valBits = [False,False,False,True,False,True,True,False], valFields = []})
+
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Word8)) (flat (11::Word8))
+Right (Value {valType = TypeCon (AbsRef (SHAKE128_48 177 244 106 73 200 248)), valName = "V11", valBits = [False,False,False,False,True,False,True,True], valFields = []})
+
+The valBits refer only to the bottom level:
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Double)) (flat 1.1)
+Right (Value {valType = TypeCon (AbsRef (SHAKE128_48 203 169 89 107 70 87)), valName = "IEEE_754_binary64", valBits = [], valFields = [Value {valType = TypeCon (AbsRef (SHAKE128_48 84 159 145 243 176 236)), valName = "Positive", valBits = [False]...
+
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy Char)) (flat 'a')
+Right (Value {valType = TypeCon (AbsRef (SHAKE128_48 6 109 181 42 241 69)), valName = "Char"...
+
+>>> decodeAbsTypeModel (absTypeModel (Proxy::Proxy String)) (flat "abc")
+Right (Value {valType = TypeApp (TypeCon (AbsRef (SHAKE128_48 184 205 19 24 113 152))) (TypeCon (AbsRef (SHAKE128_48 6 109 181 42 241 69))), valName = "Cons", valBits = [True]...
+
 -}
 decodeAbsTypeModel :: AbsTypeModel -> B.ByteString -> Decoded Value
 decodeAbsTypeModel = unflatWith . typeDecoder
@@ -45,13 +74,23 @@ type MapTypeDecoder = M.Map (Type AbsRef) (Get Value)
 typeDecoderMap :: AbsTypeModel -> MapTypeDecoder
 typeDecoderMap tm =
   let denv = M.mapWithKey (\t ct -> conDecoder denv t [] ct) (typeTree tm)
-  in denv
+   in denv
 
-conDecoder :: (Convertible name String) => MapTypeDecoder -> AbsType -> [Bool] -> ConTree name AbsRef -> Get Value
+conDecoder ::
+     (Convertible name String)
+  => MapTypeDecoder
+  -> AbsType
+  -> [Bool]
+  -> ConTree name AbsRef
+  -> Get Value
 conDecoder env t bs (ConTree l r) = do
   tag :: Bool <- decode
-  conDecoder env t (tag:bs) (if tag then r else l)
-
-conDecoder env t bs (Con cn cs) = Value t (convert cn) (reverse bs) <$> mapM (`solve` env) (fieldsTypes cs)
-
-
+  conDecoder
+    env
+    t
+    (tag : bs)
+    (if tag
+       then r
+       else l)
+conDecoder env t bs (Con cn cs) =
+  Value t (convert cn) (reverse bs) <$> mapM (`solve` env) (fieldsTypes cs)
--- src/ZM/Types.hs
+++ src/ZM/Types.hs
@@ -6,11 +6,10 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
-module ZM.Types
 
-  (
+module ZM.Types
     -- * Model
-    module Data.Model.Types
+  ( module Data.Model.Types
   , AbsTypeModel
   , AbsType
   , AbsRef(..)
@@ -49,8 +48,8 @@ module ZM.Types
   , LeastSignificantFirst(..)
   , MostSignificantFirst(..)
   , Value(..)
-  --, Label(..)
-  --, label
+  -- , Label(..)
+  -- , label
   ) where
 
 import           Control.DeepSeq
@@ -61,6 +60,7 @@ import           Data.Digest.Keccak
 import           Data.Either.Validation
 import           Data.Flat
 import           Data.Foldable
+
 -- import qualified Data.Map               as M
 import           Data.Model             hiding (Name)
 import           Data.Model.Types       hiding (Name)
@@ -77,9 +77,11 @@ type AbsType = Type AbsRef
 
 -- |A reference to an absolute data type definition, in the form of a hash of the data type definition itself
 -- data AbsRef = AbsRef (SHA3_256_6 AbsADT) deriving (Eq, Ord, Show, NFData, Generic, Flat)
-newtype AbsRef = AbsRef (SHAKE128_48 AbsADT) deriving (Eq, Ord, Show, NFData, Generic, Flat)
--- type AbsRef = SHAKE128_48 AbsADT
+newtype AbsRef =
+  AbsRef (SHAKE128_48 AbsADT)
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
+-- type AbsRef = SHAKE128_48 AbsADT
 -- |Return the absolute reference of the given value
 absRef :: AbsADT -> AbsRef
 absRef = AbsRef . shake128_48
@@ -90,17 +92,19 @@ shake128_48 = shake128_48_BS . flat
 
 -- |Return the SHAKE128_48 reference of the given ByteString
 shake128_48_BS :: B.ByteString -> SHAKE128_48 a
-shake128_48_BS bs = 
+shake128_48_BS bs =
   case B.unpack . shake_128 6 $ bs of
-    [w1,w2,w3,w4,w5,w6] -> SHAKE128_48 w1 w2 w3 w4 w5 w6
-    _ -> error "impossible"                
+    [w1, w2, w3, w4, w5, w6] -> SHAKE128_48 w1 w2 w3 w4 w5 w6
+    _                        -> error "impossible"
 
 -- |A hash of a value, the first 6 bytes of the value's SHA3-256 hash
-data SHA3_256_6 a = SHA3_256_6 Word8 Word8 Word8 Word8 Word8 Word8
+data SHA3_256_6 a =
+  SHA3_256_6 Word8 Word8 Word8 Word8 Word8 Word8
   deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 -- |A hash of a value, the first 48 bits (6 bytes) of the value's SHAKE128 hash
-data SHAKE128_48 a = SHAKE128_48 Word8 Word8 Word8 Word8 Word8 Word8
+data SHAKE128_48 a =
+  SHAKE128_48 Word8 Word8 Word8 Word8 Word8 Word8
   deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 -- CHECK: Same syntax for adt and constructor names
@@ -114,14 +118,22 @@ type AbsTypeModel = TypeModel Identifier Identifier (ADTRef AbsRef) AbsRef
 type AbsEnv = TypeEnv Identifier Identifier (ADTRef AbsRef) AbsRef
 
 -- type ADTEnv = M.Map AbsRef AbsADT
-
 -- FIX: add a custom type for Var
-
 -- |A reference inside an ADT to another ADT
-data ADTRef r = Var Word8 -- ^Variable, standing for a type
-              | Rec       -- ^Recursive reference to the ADT itself
-              | Ext r     -- ^Reference to another ADT
-  deriving (Eq, Ord, Show, NFData, Generic, Functor, Foldable, Traversable ,Flat)
+data ADTRef r
+  = Var Word8 -- ^Variable, standing for a type
+  | Rec -- ^Recursive reference to the ADT itself
+  | Ext r -- ^Reference to another ADT
+  deriving ( Eq
+           , Ord
+           , Show
+           , NFData
+           , Generic
+           , Functor
+           , Foldable
+           , Traversable
+           , Flat
+           )
 
 -- |Return an external reference, if present
 getADTRef :: ADTRef a -> Maybe a
@@ -129,27 +141,29 @@ getADTRef (Ext r) = Just r
 getADTRef _       = Nothing
 
 toTypeRef :: name -> ADTRef name -> TypeRef name
-toTypeRef _ (Var n) = TypVar n
-toTypeRef _ (Ext k) = TypRef k
-toTypeRef adtRef Rec  = TypRef adtRef
+toTypeRef _ (Var n)  = TypVar n
+toTypeRef _ (Ext k)  = TypRef k
+toTypeRef adtRef Rec = TypRef adtRef
 
 substAbsADT :: (AbsRef -> b) -> AbsADT -> ADT Identifier Identifier (TypeRef b)
 substAbsADT f adt = (f <$>) <$> (toTypeRef (absRef adt) <$> adt)
 
 -- CHECK: Is it necessary to specify a syntax for identifiers?
 -- |An Identifier, the name of an ADT
-data Identifier = Name UnicodeLetter [UnicodeLetterOrNumberOrLine]
-                | Symbol (NonEmptyList UnicodeSymbol)
-                deriving (Eq, Ord, Show, NFData, Generic, Flat)
+data Identifier
+  = Name UnicodeLetter [UnicodeLetterOrNumberOrLine]
+  | Symbol (NonEmptyList UnicodeSymbol)
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 -- instance Flat [UnicodeLetterOrNumberOrLine]
-
-instance Convertible String Identifier where
+instance Convertible String Identifier
   -- safeConvert = errorsToConvertResult asIdentifier
+                                                      where
   safeConvert = errorsToConvertResult (validationToEither . asIdentifier)
 
 instance Convertible Identifier String where
-  safeConvert (Name (UnicodeLetter h) t) = Right $ h : map (\(UnicodeLetterOrNumberOrLine s) -> s) t
+  safeConvert (Name (UnicodeLetter h) t) =
+    Right $ h : map (\(UnicodeLetterOrNumberOrLine s) -> s) t
   safeConvert (Symbol l) = Right $ map (\(UnicodeSymbol s) -> s) . toList $ l
 
 {-|Validate a string as an Identifier
@@ -169,14 +183,16 @@ Success (Symbol (Cons (UnicodeSymbol '<') (Elem (UnicodeSymbol '>'))))
 -}
 asIdentifier :: String -> Validation Errors Identifier
 asIdentifier [] = err ["identifier cannot be empty"]
-
-asIdentifier i@(h:t) = errsInContext i $
+asIdentifier i@(h:t) =
+  errsInContext i $
   if isLetter h
-  then Name <$> asLetter h <*> traverse asLetterOrNumber t
-  else Symbol . nonEmptyList <$> traverse asSymbol i
+    then Name <$> asLetter h <*> traverse asLetterOrNumber t
+    else Symbol . nonEmptyList <$> traverse asSymbol i
 
 -- |A character that is either a `UnicodeLetter`, a `UnicodeNumber` or the special character '_'
-newtype UnicodeLetterOrNumberOrLine = UnicodeLetterOrNumberOrLine Char deriving (Eq, Ord, Show, NFData, Generic, Flat)
+newtype UnicodeLetterOrNumberOrLine =
+  UnicodeLetterOrNumberOrLine Char
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 {-|
 A character that is included in one of the following Unicode classes:
@@ -186,7 +202,9 @@ TitlecaseLetter
 ModifierLetter
 OtherLetter
 -}
-newtype UnicodeLetter = UnicodeLetter Char deriving (Eq, Ord, Show, NFData, Generic, Flat)
+newtype UnicodeLetter =
+  UnicodeLetter Char
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 {-|
 A character that is included in one of the following Unicode classes:
@@ -194,7 +212,9 @@ DecimalNumber
 LetterNumber
 OtherNumber
 -}
-newtype UnicodeNumber = UnicodeNumber Char deriving (Eq, Ord, Show, NFData, Generic, Flat)
+newtype UnicodeNumber =
+  UnicodeNumber Char
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 {-|
 A character that is included in one of the following Unicode classes:
@@ -203,25 +223,30 @@ CurrencySymbol
 ModifierSymbol
 OtherSymbol
 -}
-newtype UnicodeSymbol = UnicodeSymbol Char deriving (Eq, Ord, Show, NFData, Generic, Flat)
+newtype UnicodeSymbol =
+  UnicodeSymbol Char
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
 asSymbol :: Char -> Validation Errors UnicodeSymbol
-asSymbol c | isSymbol c = ok $ UnicodeSymbol c
-           | otherwise = err [show c,"is not an Unicode Symbol"]
+asSymbol c
+  | isSymbol c = ok $ UnicodeSymbol c
+  | otherwise = err [show c, "is not an Unicode Symbol"]
 
 asLetter :: Char -> Validation Errors UnicodeLetter
-asLetter c | isLetter c = ok $ UnicodeLetter c
-           | otherwise = err [show c,"is not an Unicode Letter"]
+asLetter c
+  | isLetter c = ok $ UnicodeLetter c
+  | otherwise = err [show c, "is not an Unicode Letter"]
 
 asLetterOrNumber :: Char -> Validation Errors UnicodeLetterOrNumberOrLine
-asLetterOrNumber c | isLetter c || isNumber c || isAlsoOK c = ok $ UnicodeLetterOrNumberOrLine c
-                   | otherwise = err [show c,"is not an Unicode Letter or Number or a _"]
+asLetterOrNumber c
+  | isLetter c || isNumber c || isAlsoOK c = ok $ UnicodeLetterOrNumberOrLine c
+  | otherwise = err [show c, "is not an Unicode Letter or Number or a _"]
 
 ok :: a -> Validation e a
 ok = Success
 
 err :: [String] -> Validation Errors a
-err = Failure . (:[]) . unwords
+err = Failure . (: []) . unwords
 
 -- CHECK: IS '_' REALLY NEEDED?
 isAlsoOK :: Char -> Bool
@@ -229,56 +254,87 @@ isAlsoOK '_' = True
 isAlsoOK _   = False
 
 -- Violations of ZM model constrains
-data ZMError t = UnknownType t
-                | WrongKind {reference::t,expectedArity,actualArity::Int}
-                | MutuallyRecursive [t] deriving (Show,Functor,Foldable)
+data ZMError t
+  = UnknownType t
+  | WrongKind
+      { reference                  :: t
+      , expectedArity, actualArity :: Int
+      }
+  | MutuallyRecursive [t]
+  deriving (Show, Functor, Foldable)
 
 -- |A generic value (used for dynamic decoding)
-data Value = Value {valType::AbsType -- ^Type
-                   ,valName::String  -- ^Constructor name (duplicate info if we have abstype)
-                   ,valBits::[Bool]    -- ^Bit encoding/constructor id
+data Value =
+  Value
+    { valType   :: AbsType -- ^Type
+    , valName   :: String -- ^Constructor name (duplicate info if we have abstype)
+    , valBits   :: [Bool] -- ^Bit encoding/constructor id
                    -- TODO: add field names (same info present in abstype)
-                   ,valFields::[Value]  -- ^Values to which the constructor is applied, if any
-                   } deriving  (Eq,Ord,Show,NFData, Generic, Flat)
+    , valFields :: [Value] -- ^Values to which the constructor is applied, if any
+    }
+  deriving (Eq, Ord, Show, NFData, Generic, Flat)
 
--- -- |An optionally labeled value
--- data Label a label = Label a (Maybe label) deriving (Eq, Ord, Show, NFData, Generic, Flat)
-
+-- -- -- |An optionally labeled value
+-- data Label a label =
+--   Label a (Maybe label)
+--   deriving (Eq, Ord, Show, NFData, Generic, Flat)
 -- label :: (Functor f, Ord k) => M.Map k a -> (a -> l) -> f k -> f (Label k l)
 -- label env f o = (\ref -> Label ref (f <$> M.lookup ref env)) <$> o
-
 type TypedDecoded a = Either TypedDecodeException a
 
 -- |An exception thrown if the decoding of a type value fails
-data TypedDecodeException = UnknownMetaModel AbsType
-                            | WrongType {expectedType::AbsType,actualType::AbsType}
-                            | DecodeError DecodeException deriving (Show,Eq,Ord)
+data TypedDecodeException
+  = UnknownMetaModel AbsType
+  | WrongType
+      { expectedType :: AbsType
+      , actualType   :: AbsType
+      }
+  | DecodeError DecodeException
+  deriving (Show, Eq, Ord)
 
 instance Exception TypedDecodeException
 
 -- newtype LocalName = LocalName Identifier deriving (Eq, Ord, Show, NFData, Generic, Flat)
-
 -- Flat instances for data types in the 'model' package
-instance (Flat adtName, Flat consName, Flat inRef, Flat exRef,Ord exRef) => Flat (TypeModel adtName consName inRef exRef)
-instance (Flat a,Flat b,Flat c) => Flat (ADT a b c)
-instance (Flat a,Flat b) => Flat (ConTree a b)
+instance (Flat adtName, Flat consName, Flat inRef, Flat exRef, Ord exRef) =>
+         Flat (TypeModel adtName consName inRef exRef)
+
+instance (Flat a, Flat b, Flat c) => Flat (ADT a b c)
+
+instance (Flat a, Flat b) => Flat (ConTree a b)
+
 -- instance (Flat a,Flat b) => Flat [(a,Type b)]
 -- instance Flat a => Flat [Type a]
 instance Flat a => Flat (Type a)
+
 instance Flat a => Flat (TypeRef a)
 
 -- Model instances
-instance (Model a,Model b,Model c) => Model (ADT a b c)
-instance (Model a,Model b) => Model (ConTree a b)
+instance (Model a, Model b, Model c) => Model (ADT a b c)
+
+instance (Model a, Model b) => Model (ConTree a b)
+
 instance Model a => Model (ADTRef a)
+
 instance Model a => Model (Type a)
+
 instance Model a => Model (TypeRef a)
-instance (Model adtName, Model consName, Model inRef, Model exRef) => Model (TypeModel adtName consName inRef exRef)
+
+instance (Model adtName, Model consName, Model inRef, Model exRef) =>
+         Model (TypeModel adtName consName inRef exRef)
+
 instance Model Identifier
+
 instance Model UnicodeLetter
+
 instance Model UnicodeLetterOrNumberOrLine
+
 instance Model UnicodeSymbol
+
 instance Model a => Model (SHA3_256_6 a)
+
 instance Model a => Model (SHAKE128_48 a)
+
 instance Model AbsRef
+
 instance Model a => Model (PostAligned a)
