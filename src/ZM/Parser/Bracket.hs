9939f680b2bc923371e2869a75a9f1f6dc4d330a
Pasqualino Titto Assini <tittoassini@gmail.com>
2024-06-25 18:59:38 +0200

    added pretty instance

--- src/ZM/Parser/Bracket.hs
+++ src/ZM/Parser/Bracket.hs
@@ -5,13 +5,13 @@ module ZM.Parser.Bracket (
     bracket,
 ) where
 
-import Data.Text (Text, pack)
+import Data.Maybe (fromMaybe)
+import Data.Text (Text)
 import Text.Megaparsec
 import Text.Megaparsec.Char
-import qualified Text.Megaparsec.Char.Lexer as L
-import Text.Megaparsec.Debug
 import ZM.Parser.Lexer
 import ZM.Parser.Types
+import ZM.Pretty
 
 {- List of values between brackets, space separated, with an optional operand/modifier
 >>> import ZM.Parser.Lexer
@@ -40,20 +40,29 @@ Just (Bracket {open = '[', op = Nothing, values = ["a","a"]})
 Just (Bracket {open = '[', op = Nothing, values = ""})
 -}
 bracket :: Parser e -> Parser (Bracket e)
-bracket pe = do
+bracket pe = lexeme $ do
     (o, c) <- choice (map (\oc@(o, _) -> oc <$ char o) brackets)
     msym <- optional sym
     vs <- many pe
     _ <- maybe (string "") string msym
     _ <- char c
-    return $ Bracket o msym vs
+    return $ Bracket o c msym vs
 
 brackets :: [(Char, Char)]
 brackets = [('{', '}'), ('[', ']')] -- ('<','>'),('«','»')]
 
 data Bracket e = Bracket
-    { open :: Char
+    { open, close :: Char
     , op :: Maybe Text
     , values :: [e]
     }
     deriving (Show, Eq, Ord)
+
+{-
+>>> prettyShow <$> parseMaybe (bracket signedInt) "{%%11 22%%}"
+Just "{%%11 22%%}"
+-}
+instance (Pretty e) => Pretty (Bracket e) where
+    pPrint (Bracket open close mop vs) =
+        let op = txt . fromMaybe "" $ mop
+         in chr open <> op <> hsep (map pPrint vs) <> op <> chr close
